7274f4a917e35ef8cfc42e166d39467bd980f1d3
Alexander Tokmakov <tokmakovaa95@gmail.com>
2026-05-25 11:37:12 +0500

    My first commit

---
+++ data/ingredients_data.py
@@ -0,0 +1,7 @@
+class Ingredients:
+    
+    correct_ingredients_data = {
+        "ingredients": ["61c0c5a71d1f82001bdaaa6d", "61c0c5a71d1f82001bdaaa70"]}
+
+    incorrect_ingredients_data = {
+        "ingredients": ["60d3b41abdacab0026a733777", "609646e4dc916e00276b28777"]}
