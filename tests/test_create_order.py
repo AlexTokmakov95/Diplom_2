66c6bcec3d01c83ebab3e3d3aac6feb49fd6a9ea
Alexander Tokmakov <tokmakovaa95@gmail.com>
2026-05-28 10:51:17 +0500

    My second commit

--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -11,23 +11,27 @@ class TestCreateOrder:
     @allure.title('Создание заказа с авторизацией')
     def test_create_order_with_authorization(self, create_user):
         token = {'Authorization': create_user[3]}
-        r = requests.post(Urls.url_create_order, headers=token, data=Ingredients.correct_ingredients_data)
+        with allure.step("Отправляем запрос на создание заказа"):
+            r = requests.post(Urls.url_create_order, headers=token, data=Ingredients.correct_ingredients_data)
         assert r.status_code == 200 and r.json().get("success") is True
 
     @allure.description('Успешное создание заказа с неавторизованным пользователем')
     @allure.title('Создание заказа без авторизации')
     def test_create_order_not_authorization(self):
-        r = requests.post(Urls.url_create_order, data=Ingredients.correct_ingredients_data)
+        with allure.step("Отправляем запрос на создание заказа"):
+            r = requests.post(Urls.url_create_order, data=Ingredients.correct_ingredients_data)
         assert r.status_code == 200 and r.json().get("success") is True  
 
     @allure.description('Ошибка при создании заказа без ингредиентов')
     @allure.title('Создание заказа без ингредиентов')
     def test_create_order_without_ingridients(self):
-        r = requests.post(Urls.url_create_order)
+        with allure.step("Отправляем запрос на создание заказа"):
+            r = requests.post(Urls.url_create_order)
         assert r.status_code == 400 and r.json()['message'] == "Ingredient ids must be provided"
 
     @allure.description('Ошибка при создании заказа с неверным хешем ингредиентов')
     @allure.title('Создание заказа с неверным хешем ингредиентов')
     def test_create_order_invalid_hash_ingridient(self):
-        response = requests.post(Urls.url_create_order, headers=Urls.headers, json=Ingredients.incorrect_ingredients_data)
+        with allure.step("Отправляем запрос на создание заказа"):
+            response = requests.post(Urls.url_create_order, headers=Urls.headers, json=Ingredients.incorrect_ingredients_data)
         assert response.status_code == 500 and 'Internal Server Error' in response.text         
--- tests/test_login_user.py
+++ tests/test_login_user.py
@@ -9,11 +9,13 @@ class TestLoginUser:
     @allure.description('Успешный логин под существующим пользователем')
     @allure.title('Логин пользователя под существующим пользователем')
     def test_login_user_success(self):
-        response = requests.post(Urls.url_login_user, data = User.data_correct)
+        with allure.step("Отправляем запрос на логин пользователя"):
+            response = requests.post(Urls.url_login_user, data = User.data_correct)
         assert response.status_code == 200 and response.json().get('success') == True
 
     @allure.description('Ошибка при попытке входа с неверным логином и паролем')
     @allure.title('Логин пользователя с неверным логином и паролем')
     def test_login_with_uncorrect_data_error(self):
-        response = requests.post(Urls.url_login_user, data = User.data_negative)
+        with allure.step("Отправляем запрос на логин пользователя"):
+            response = requests.post(Urls.url_login_user, data = User.data_negative)
         assert response.status_code == 401 and response.json().get('success') == False
